2d012daf47a30e476b247403f5b6ce4c6783f252
example <example@example.com>
2022-08-02 18:44:24 +0700

    fix

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import 'firebase/compat/analytics';
+import firebase from 'firebase/app';
+
+import 'firebase/analytics';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
